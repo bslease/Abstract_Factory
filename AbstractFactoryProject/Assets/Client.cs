6a9d8c39c11c28d656064746b0b257f3095a8a89
bslease <bslease@gmail.com>
2020-01-26 09:42:03 -0700

    Initial Commit

---
+++ AbstractFactoryProject/Assets/Client.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    public int NumberOfWheels;
+    public bool Engine;
+    public int Passengers;
+    public bool Cargo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // validate our data
+        NumberOfWheels = Mathf.Max(NumberOfWheels, 1);
+        Passengers = Mathf.Max(Passengers, 1);
+        Engine = Cargo;
+
+        VehicleRequirements requirements = new VehicleRequirements();
+        requirements.NumberOfWheels = NumberOfWheels;
+        requirements.Engine = Engine;
+        requirements.Passengers = Passengers;
+
+        //IVehicle v = new Unicycle();
+        IVehicle v = GetVehicle(requirements);
+        Debug.Log(v);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private static IVehicle GetVehicle(VehicleRequirements requirements)
+    {
+        // based on requirements.Engine
+        // choose a motorvehicle factory or a cycle factory
+        // call create on the factory to get an appropriate vehicle
+        // and return it
+
+        VehicleFactory factory = new VehicleFactory();
+
+        if (requirements.Engine)
+        {
+            return factory.MotorVehicleFactory().Create(requirements);
+        }
+
+        return factory.CycleFactory().Create(requirements);
+    }
+}
---
+++ AbstractFactoryProject/Assets/VehicleFactory.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IVehicleFactory
+{
+    IVehicle Create(VehicleRequirements requirements);
+}
+
+public class CycleFactory : IVehicleFactory
+{
+    public IVehicle Create(VehicleRequirements requirements)
+    {
+        switch (requirements.Passengers)
+        {
+            case 1:
+                if (requirements.NumberOfWheels == 1) return new Unicycle();
+                return new Bicycle();
+            default:
+                return new Bicycle();
+        }
+    }
+}
+
+public class MotorVehicleFactory : IVehicleFactory
+{
+    public IVehicle Create(VehicleRequirements requirements)
+    {
+        switch (requirements.Passengers)
+        {
+            case 1:
+                return new Motorbike();
+            default:
+                return new Truck();
+        }
+    }
+}
+
+public abstract class AbstractVehicleFactory
+{
+    public abstract IVehicleFactory CycleFactory();
+    public abstract IVehicleFactory MotorVehicleFactory();
+}
+
+
+
+public class VehicleFactory : AbstractVehicleFactory
+{
+    public override IVehicleFactory CycleFactory()
+    {
+        return new CycleFactory();
+    }
+
+    public override IVehicleFactory MotorVehicleFactory()
+    {
+        return new MotorVehicleFactory();
+    }
+}
+
+
